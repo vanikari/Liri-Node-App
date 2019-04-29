1e952aaf0c79581d955bd80f792c3e2bb451760d
Vani <vani.karikalan@gmail.com>
2019-04-29 12:29:04 -0700

    npm install dotenv

--- keys.js
+++ keys.js
@@ -1,7 +1,9 @@
-js
+
 console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+console.log(process.env.SPOTIFY_ID)
--- liri.js
+++ liri.js
@@ -1,10 +1,7 @@
-js
 require("dotenv").config();
 
-js
 var keys = require("./keys.js");
 
-js
 var spotify = new Spotify(keys.spotify);
 
 const action = process.argv[2];
