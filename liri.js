0b7007374a6debaa25957a0470adb9a6a1ef1b0a
Vani <vani.karikalan@gmail.com>
2019-04-29 11:49:27 -0700

    initial liri.js

---
+++ liri.js
@@ -0,0 +1,29 @@
+js
+require("dotenv").config();
+
+js
+var keys = require("./keys.js");
+
+js
+var spotify = new Spotify(keys.spotify);
+
+const action = process.argv[2];
+
+switch (action) {
+
+    case "concert-this":
+
+    break;
+
+    case "spotify-this-song":
+    
+    break;
+
+    case "movie-this":
+    
+    break;
+
+    case "do-what-it-says":
+    
+    break;
+}
